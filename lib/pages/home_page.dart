6a0dd69c9fc5f5f8cf19e462968c1e7eea64f6cd
Nelson Bryan <201913582B@utea.edu.pe>
2022-12-09 19:14:13 -0500

    desarrollo del home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,12 +13,100 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Data"),
-          onPressed: (() {
-            getDataPokemon();
-          }),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 12.0,
+                ),
+                const Text(
+                  "Pokedex",
+                  style: TextStyle(
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12.0,
+                  crossAxisSpacing: 12.0,
+                  childAspectRatio: 1.35,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff4CCFB3),
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: -20,
+                            right: -20,
+                            child: Image.asset(
+                              "assets/images/pokeball.png",
+                              height: 120.0,
+                              color: Colors.white.withOpacity(0.2),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 12,
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Bulbasaur",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                Container(
+                                  margin:
+                                      const EdgeInsets.symmetric(vertical: 6.0),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 4,
+                                    horizontal: 14,
+                                  ),
+                                  child: Text("Grass"),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.2),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color:
+                                                Colors.black.withOpacity(0.04),
+                                            offset: const Offset(4, 4),
+                                            blurRadius: 12.0)
+                                      ]),
+                                )
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Image.network(
+                              "http://www.serebii.net/pokemongo/pokemon/001.png",
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
